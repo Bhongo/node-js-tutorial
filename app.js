3b3e2dd6201c2f837ee00d7519059d3ce717c974
Bhongo <sinxo.bhongolethu@gmail.com>
2022-05-31 16:31:37 +0200

    Using the global scope to the directory and filename

--- app.js
+++ app.js
@@ -7,33 +7,44 @@
 // Globals in Node: It gives us access to certain methods
 // ----
 
-setTimeout(() => {
-  console.log('Hi there after 3 seconds');
-}, 3000); // 3 seconds - displays method (console.log()) after 3 seconds
+// setTimeout(() => {
+//   console.log('Hi there after 3 seconds');
+// }, 3000); // 3 seconds - displays method (console.log()) after 3 seconds
 
-setInterval(() => {
-  console.log('Hi there');
-}, 2000); // displays the message every 2 seconds
+// setInterval(() => {
+//   console.log('Hi there');
+// }, 2000); // displays the message every 2 seconds
 
 //====================================
 // == Example 2 using a variable == //
 //====================================
 
-let count = 0; // declaring a variable
+// let count = 0; // declaring a variable
 
-setInterval(() => {
-  count = count + 2;
-  console.log(`${count} seconds have passed!`);
-}, 2000); // every 2 seconds
+// setInterval(() => {
+//   count = count + 2;
+//   console.log(`${count} seconds have passed!`);
+// }, 2000); // every 2 seconds
 
-// Ending a process using ClearInterval
+// // Ending a process using ClearInterval
 
-let newcount = 0;
+// let newcount = 0;
 
-const timer = setInterval(() => {
-  newcount = newcount + 2;
-  console.log(`${newcount} seconds have passed!`);
-  if (newcount > 6) {
-    clearInterval(timer);
-  }
-}, 2000); // every 2 seconds
+// const timer = setInterval(() => {
+//   newcount = newcount + 2;
+//   console.log(`${newcount} seconds have passed!`);
+//   if (newcount > 6) {
+//     clearInterval(timer);
+//   }
+// }, 2000); // every 2 seconds
+
+//=======================================================
+// == Example 3 using node to get my file directory == //
+//=======================================================
+
+console.log(__dirname); // method to get the directory (standard way)
+
+var myDir = __dirname;
+console.log(myDir); // the dynamic way of getting the directory
+
+console.log(__filename); // gets the file name and directory
