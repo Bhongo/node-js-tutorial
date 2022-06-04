54f58460b4381a7487d1a7106e17b20cfd081a9a
Bhongo <sinxo.bhongolethu@gmail.com>
2022-06-04 17:45:18 +0200

    two new views with the templates added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Using the Express Module in NodeJs
 
-const express = require('express');  // bring in the module
+const express = require('express'); // bring in the module
 
 const app = express(); // setting up the express application
 
@@ -8,20 +8,27 @@ app.set('view engine', 'ejs'); // allowing the page to bring in HTML
 
 // Using the HTTP Express methods
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   // res.send('Home page');
-  res.sendFile(__dir + '/index.html');
+  // res.sendFile(__dir + '/index.html');
+  res.render('index'); // No need to specify the path, because it know automatiocally to go the views folder
 });
 
-app.get('/about', function(req, res) { // Express knows the data is a string thus, no need for content type
+app.get('/about', function (req, res) {
+  // Express knows the data is a string thus, no need for content type
   // res.send('About page');
-  res.sendFile(__dir + '/about.html');
+  // res.sendFile(__dir + '/about.html');
+  res.render('about');
 });
 
-app.get('/profile/:name', function(req, res){
+app.get('/profile/:name', function (req, res) {
   // res.send('The profile name is ' + req.params.name);
-  const data = {age: 30, job: 'Node Developer', friends: ['sarah', 'jane', 'willma']}
-  res.render('profile', {person: req.params.name, data});
+  const data = {
+    age: 30,
+    job: 'Node Developer',
+    friends: ['sarah', 'jane', 'willma'],
+  };
+  res.render('profile', { person: req.params.name, data });
 });
 
 app.listen(3000); // the port to use for running this app in the browser
