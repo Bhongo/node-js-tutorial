35da9111071edd943540fe7aeb8ea363dca4c49d
Bhongo <sinxo.bhongolethu@gmail.com>
2022-06-04 16:27:34 +0200

    2 files modified

--- app.js
+++ app.js
@@ -10,15 +10,18 @@ app.set('view engine', 'ejs'); // allowing the page to bring in HTML
 
 app.get('/', function(req, res) {
   // res.send('Home page');
-  res.sendFile()
+  res.sendFile(__dir + '/index.html');
 });
 
 app.get('/about', function(req, res) { // Express knows the data is a string thus, no need for content type
-  res.send('About page');
+  // res.send('About page');
+  res.sendFile(__dir + '/about.html');
 });
 
 app.get('/profile/:name', function(req, res){
-  res.send('The profile name is ' + req.params.name);
+  // res.send('The profile name is ' + req.params.name);
+  const data = {age: 30, job: 'Node Developer', }
+  res.render('profile', {person: req.params.name, data});
 });
 
 app.listen(3000); // the port to use for running this app in the browser
